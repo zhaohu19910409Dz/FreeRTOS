073e4c40c72f99b20d5a6ce3e20769042d3e9a5b
赵珮宇 <zhaopeiyu@zhaopeiyudeAir.lan>
2020-09-13 17:58:51 +0800

    add register logic

--- ARunningMan/app/src/main/java/john/zhao/arunningman/app/App.java
+++ ARunningMan/app/src/main/java/john/zhao/arunningman/app/App.java
@@ -1,6 +1,10 @@
 package john.zhao.arunningman.app;
 
+import android.Manifest;
 import android.app.Application;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
 
 import com.baidu.mapapi.CoordType;
 import com.baidu.mapapi.SDKInitializer;
@@ -13,6 +17,7 @@ public class App extends Application {
     private String APP_KEY = "29aebf805084336743316d96de80c307";
     @Override
     public void onCreate() {
+        Log.d("John", "App onCreate");
         super.onCreate();
         //bmob iniy
         Bmob.initialize(this, APP_KEY);
